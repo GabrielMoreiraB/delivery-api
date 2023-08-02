b4b2669f64704d8ab012897ece66ae8c9d1ebf4e
Gabriel Moreira <gabrielgbm97@gmail.com>
2023-08-02 19:19:21 -0300

    constructor endpoint GET PUT DELETE

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -28,11 +28,89 @@ router.post('/', async (req, res, next) => {
     await writeFile(global.fileName, JSON.stringify((data, null, 2)));
 
     res.send(pedido);
-    logger.info(`POST /account - ${JSON.stringify(pedido)}`);
+    logger.info(`POST /pedido - ${JSON.stringify(pedido)}`);
   } catch (err) {
     next(err)
   }
 });
+
+router.put('/', async (req, res, next) => {
+  try{
+      let pedido = req.body
+
+      if(!pedido.cliente || !pedido.produto || !pedido.valor == null || !pedido.entregue) {
+        throw new Error("Formato invalido, cliente valor e produto são obrigatorios")
+      }
+      const data = JSON.parse(await readFile(global.fileName));
+      const index = data.pedidos.findIndex(item => item.id === pedido.id);
+
+      if(index === -1) throw new Error("Registro não encontrado");
+
+
+      data.pedidos[index].cliente = pedido.cliente;
+      data.pedidos[index].produto = pedido.produto;
+      data.pedidos[index].valor = pedido.valor;
+      data.pedidos[index].entregue = pedido.entregue;
+
+      await writeFile(global.fileName, JSON.stringify(data));
+      logger.info(`PUT /pedido - ${JSON.stringify(pedido)}`);
+      res.send(pedido);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.patch("/updatePedido", async (req, res, next) => {
+  try{
+    let pedido = req.body
+
+    if(!pedido.id || typeof(pedido.entregue) !== 'boolean' ) {
+      throw new Error("Formato invalido, id e statys de entregue são necessarios")
+    }
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.pedidos.findIndex(item => item.id === pedido.id);
+
+    if(index === -1) throw new Error("Registro não encontrado");
+
+    data.pedidos[index].entregue = pedido.entregue;
+
+    await writeFile(global.fileName, JSON.stringify(data));
+    logger.info(`PATCH /pedido/updateBalance - ${JSON.stringify(pedido)}`);
+    res.send(data.pedidos[index]);
+} catch (err) {
+  next(err)
+}
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.pedidos = data.pedidos.filter(
+      pedido => pedido.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data));
+    logger.info(`DELETE /pedido/:id - ${req.params.id}`);
+    res.send(`Pedido excluido com sucesso`);
+    res.end();
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const pedido = data.pedidos.find(
+      pedido => pedido.id === parseInt(req.params.id)
+    );
+    logger.info(`GET /pedido/:id `);
+    res.send(pedido);
+  } catch (err) {
+    next(err)
+  }
+});
+
 /* 
 router.get('/', async (req, res, next) => {
   try {
@@ -73,52 +151,7 @@ router.delete('/:id', async (req, res, next) => {
   }
 });
 
-router.put('/', async (req, res, next) => {
-  try{
-      let account = req.body
 
-      if(!account.name || !account.balance == null) {
-        throw new Error("Formato invalido, name e balance obrigatorios")
-      }
-      const data = JSON.parse(await readFile(Dlobal.fileName));
-      const index = data.accounts.findIndex(item => item.id === account.id);
-
-      if(index === -1) throw new Error("Registro não encontrado");
-
-
-      data.accounts[index].name = account.name;
-      data.accounts[index].balance = account.balance;
-
-      await writeFile(global.fileName, JSON.stringify((data, null, 2)));
-      logger.info(`PUT /account - ${JSON.stringify(account)}`);
-      res.send(account);
-  } catch (err) {
-    next(err)
-  }
-});
-
-router.patch("/updateBalance", async (req, res, next) => {
-  try{
-    let account = req.body
-
-    if(!account.id || !account.balance == null) {
-      throw new Error("Formato invalido, name e balance obrigatorios")
-    }
-
-    const data = JSON.parse(await readFile(global.fileName));
-    const index = data.accounts.findIndex(item => item.id === account.id);
-
-    if(index === -1) throw new Error("Registro não encontrado");
-
-    data.accounts[index].balance = account.balance;
-
-    await writeFile(global.fileName, JSON.stringify(data));
-    logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`);
-    res.send(data.accounts[index]);
-} catch (err) {
-  next(err)
-}
-});
  */
 router.use((err, req, res, next)=> {
   global.logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
